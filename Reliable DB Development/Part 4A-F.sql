fc7301eceb4cc32fbf180d2812763f26f4ac6a06
ridzrgt <ridz_rigat@dlsu.edu.ph>
2024-11-11 22:20:16 +0800

    Add files via upload

---
+++ Reliable DB Development/Part 4A-F.sql
@@ -0,0 +1,41 @@
+-- Part 4A-F
+-- Ridz Rigat
+
+DROP EVENT IF EXISTS cancel_unshipped_orders_event;
+DELIMITER $$
+CREATE EVENT cancel_unshipped_orders_event
+ON SCHEDULE 
+EVERY 10 SECOND 
+DO
+BEGIN
+    UPDATE orders
+    SET 
+        status = 'Cancelled',
+        comments = 'Automatically cancelled by system due to non-shipping within one week.'
+    WHERE 
+        status != 'Shipped' 
+        AND shippedDate IS NULL 
+        AND orderDate <= DATE_SUB(CURDATE(), INTERVAL 7 DAY);
+END $$
+DELIMITER ;
+
+SHOW PROCESSLIST;
+SHOW EVENTS;
+
+SET GLOBAL EVENT_SCHEDULER = ON;
+SET GLOBAL EVENT_SCHEDULER = OFF;
+
+SHOW VARIABLES LIKE 'event_scheduler';
+																
+ALTER EVENT cancel_unshipped_orders_event DISABLE;
+ALTER EVENT cancel_unshipped_orders_event ENABLE;
+
+-- FOR CHECKING OF THE ORDERS THAT ARE NOT SHIPPED
+SELECT * FROM orders
+WHERE 
+    status != 'Shipped' 
+    AND shippedDate IS NULL 
+    AND orderDate <= DATE_SUB(CURDATE(), INTERVAL 7 DAY);
+
+
+
