2fcf2eceb9de639a153437d369db0d49705023f6
ridzrgt <ridz_rigat@dlsu.edu.ph>
2024-11-12 00:38:50 +0800

    Update 4Bd-Rigat-DBADM-103.sql

--- Reliable DB Development/4Bd-Rigat-DBADM-103.sql
+++ Reliable DB Development/4Bd-Rigat-DBADM-103.sql
@@ -14,3 +14,31 @@ CREATE TRIGGER `current_products_BEFORE_UPDATE` BEFORE UPDATE ON `current_produc
     
 END $$
 DELIMITER ;
+
+
+-- should not update since you cannot go modify product categories.
+UPDATE current_products
+SET product_type = 'W'
+WHERE productCode = 'S10_1678'; 
+
+-- should not update since you cannot go modify product categories.
+UPDATE current_products
+SET product_type = 'W'
+WHERE productCode = 'S10_1949'; 
+
+-- should work but no changes since the product_type is already set to 'R'
+UPDATE current_products
+SET product_type = 'R'
+WHERE productCode = 'S10_1949'; 
+
+-- should work since the trigger is in the update only
+INSERT INTO products(productCode, productName, productScale, productVendor, productDescription, buyPrice, product_category, latest_audituser, latest_authorizinguser, latest_activityreason, latest_activitymethod)
+VALUES('S10_0001', 'ridznew', '1:712', 'ridztest2', 'testdesc', 33.21, 'C',  NULL, NULL, NULL, NULL);
+
+INSERT INTO current_products (productCode, product_type, quantityInStock)
+VALUES ('S10_0001', 'W', 1000);
+
+-- should not update since you cannot modify product categories 
+UPDATE current_products
+SET product_type = 'R'
+WHERE productCode = 'S10_0001'; 
