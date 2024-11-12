8d833a8db773201328970c1bc3f1e4c4f98ed76f
ridzrgt <ridz_rigat@dlsu.edu.ph>
2024-11-12 22:42:55 +0800

    Update 4Cf-Rigat-DBADM-103.sql

--- Reliable DB Development/4Cf-Rigat-DBADM-103.sql
+++ Reliable DB Development/4Cf-Rigat-DBADM-103.sql
@@ -14,9 +14,21 @@ BEGIN
         UPDATE salesRepAssignments
         SET endDate = CURDATE()
         WHERE employeeNumber = NEW.employeeNumber
-          AND endDate IS NULL;
+          AND endDate IS NULL; -- Ensures only active assignments are affected
     END IF;
 END $$
 DELIMITER ;
 
 SHOW TRIGGERS;
+
+-- test1
+-- should update the endDate to CURDATE if the endDate is null, it means assignment is still active
+UPDATE employees
+SET employee_type = 'N'
+WHERE employeeNumber = 1188;
+
+-- test2
+-- should not work since 1056 is not a salesRep
+UPDATE employees
+SET employee_type = 'S'
+WHERE employeeNumber = 1056;
