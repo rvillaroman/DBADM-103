7e757b5e9c89afab04de373035c3baf9bf285ed5
ridzrgt <ridz_rigat@dlsu.edu.ph>
2024-11-12 22:16:59 +0800

    Update 4Bd-Rigat-DBADM-103.sql

--- Reliable DB Development/4Bd-Rigat-DBADM-103.sql
+++ Reliable DB Development/4Bd-Rigat-DBADM-103.sql
@@ -15,30 +15,33 @@ CREATE TRIGGER `4BD_current_products_BEFORE_UPDATE` BEFORE UPDATE ON `current_pr
 END $$
 DELIMITER ;
 
-
+-- test1
 -- should not update since you cannot go modify product categories.
-UPDATE current_products
-SET product_type = 'W'
-WHERE productCode = 'S10_1678'; 
+-- UPDATE current_products
+-- SET product_type = 'W'
+-- WHERE productCode = 'S10_1678'; 
 
+-- test2
 -- should not update since you cannot go modify product categories.
-UPDATE current_products
-SET product_type = 'W'
-WHERE productCode = 'S10_1949'; 
+-- UPDATE current_products
+-- SET product_type = 'W'
+--WHERE productCode = 'S10_1949'; 
 
+-- test3
 -- should work but no changes since the product_type is already set to 'R'
-UPDATE current_products
-SET product_type = 'R'
-WHERE productCode = 'S10_1949'; 
+-- UPDATE current_products
+-- SET product_type = 'R'
+-- WHERE productCode = 'S10_1949'; 
 
+-- test4
 -- should work since the trigger is in the update only
-INSERT INTO products(productCode, productName, productScale, productVendor, productDescription, buyPrice, product_category, latest_audituser, latest_authorizinguser, latest_activityreason, latest_activitymethod)
-VALUES('S10_0001', 'ridznew', '1:712', 'ridztest2', 'testdesc', 33.21, 'C',  NULL, NULL, NULL, NULL);
+-- INSERT INTO products(productCode, productName, productScale, productVendor, productDescription, buyPrice, product_category, latest_audituser, latest_authorizinguser, latest_activityreason, latest_activitymethod)
+-- VALUES('S10_0001', 'ridznew', '1:712', 'ridztest2', 'testdesc', 33.21, 'C',  NULL, NULL, NULL, NULL);
+-- INSERT INTO current_products (productCode, product_type, quantityInStock)
+-- VALUES ('S10_0001', 'W', 1000);
 
-INSERT INTO current_products (productCode, product_type, quantityInStock)
-VALUES ('S10_0001', 'W', 1000);
-
+-- test5
 -- should not update since you cannot modify product categories 
-UPDATE current_products
-SET product_type = 'R'
-WHERE productCode = 'S10_0001'; 
+-- UPDATE current_products
+-- SET product_type = 'R'
+-- WHERE productCode = 'S10_0001'; 
