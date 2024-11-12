aa7b32f1581d68cae9f76cbc69b00f6c7f374b11
ridzrgt <ridz_rigat@dlsu.edu.ph>
2024-11-12 22:27:31 +0800

    Update 4Cf-Rigat-DBADM-103.sql

--- Reliable DB Development/4Cf-Rigat-DBADM-103.sql
+++ Reliable DB Development/4Cf-Rigat-DBADM-103.sql
@@ -1,19 +1,22 @@
 -- Part 4C-F
 -- Ridz Rigat
 
-DROP TRIGGER IF EXISTS 4CF_update_sales_rep_assignment_end_date;
+ALTER TABLE employees
+MODIFY employee_type ENUM('S', 'SM', 'N', 'IM') NULL;
+
+DROP TRIGGER IF EXISTS update_sales_rep_assignment_end_date;
 DELIMITER $$
-CREATE TRIGGER 4CF_update_sales_rep_assignment_end_date
+CREATE TRIGGER update_sales_rep_assignment_end_date
 BEFORE UPDATE ON employees
 FOR EACH ROW
 BEGIN
-    IF NEW.employee_type = 'N' OR NEW.employee_type = 'SM' OR NEW.employee_type = 'IM' AND OLD.employee_type = 'S' THEN
+    IF (OLD.employee_type = 'S') AND (NEW.employee_type = 'N' OR NEW.employee_type = 'SM' OR NEW.employee_type = 'IM') THEN
         UPDATE salesRepAssignments
         SET endDate = CURDATE()
-        WHERE employeeNumber = NEW.employeeNumber;
+        WHERE employeeNumber = NEW.employeeNumber
+          AND endDate IS NULL; -- Ensures only active assignments are affected
     END IF;
 END $$
-
 DELIMITER ;
 
 SHOW TRIGGERS;
