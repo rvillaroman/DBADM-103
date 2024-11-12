bafa2e1804598cf8a971637f7cc82afa850868a4
ridzrgt <ridz_rigat@dlsu.edu.ph>
2024-11-12 22:18:03 +0800

    Update 4Af-Rigat-DBADM-103.sql

--- Reliable DB Development/4Af-Rigat-DBADM-103.sql
+++ Reliable DB Development/4Af-Rigat-DBADM-103.sql
@@ -38,36 +38,42 @@ WHERE
     AND orderDate <= DATE_SUB(CURDATE(), INTERVAL 7 DAY);
 
 
-
+-- test1
 -- working since requiredDate data is more than 3 days of the order date and the orderdate is automaticall set to  CURDATE due to 4AA
 -- status will be set to 'Cancelled' and comments will be set to 'Automatically cancelled by system due to non-shipping within one week.'
-INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
-VALUES (1001, '2024-10-11', CURDATE() + INTERVAL 4 DAY, NULL, 'Processing', NULL, 119);
+-- INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
+-- VALUES (1001, '2024-10-11', CURDATE() + INTERVAL 4 DAY, NULL, 'Processing', NULL, 119);
 
+-- test2
 -- working since requiredDate data is more than 3 days of the order date
 -- status will be set to 'Cancelled' and comments will be set to 'Automatically cancelled by system due to non-shipping within one week.'
-INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
-VALUES (1002, CURDATE(), CURDATE() + INTERVAL 5 DAY, NULL, 'Processing', NULL, 119);
+-- INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
+-- VALUES (1002, CURDATE(), CURDATE() + INTERVAL 5 DAY, NULL, 'Processing', NULL, 119);
 
+-- test3
 -- working since requiredDate data is more than 3 days of the order date
 -- status will be set to 'Cancelled' and comments will be set to 'Automatically cancelled by system due to non-shipping within one week.'
-INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
-VALUES (1002, CURDATE(), CURDATE() + INTERVAL 4 DAY, NULL, 'Processing', NULL, 119);
+-- INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
+-- VALUES (1002, CURDATE(), CURDATE() + INTERVAL 4 DAY, NULL, 'Processing', NULL, 119);
 
+-- test4
 -- working since requiredDate data is more than 3 days of the order date, status will be set to In Process due to 4AA. 
 -- status will be set to 'Cancelled' and comments will be set to 'Automatically cancelled by system due to non-shipping within one week.'
-INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
-VALUES (1002, CURDATE(), CURDATE() + INTERVAL 4 DAY, NULL, 'Cancelled', 'cancelled by the user', 119);
+-- INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
+-- VALUES (1002, CURDATE(), CURDATE() + INTERVAL 4 DAY, NULL, 'Cancelled', 'cancelled by the user', 119);
 
+-- test5
 -- will not work due to 4AA before insert restriction with the 3 day rule.
-INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
-VALUES (1002, CURDATE(), CURDATE() + INTERVAL 3 DAY, NULL, 'Processing', NULL, 119);
+-- INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
+-- VALUES (1002, CURDATE(), CURDATE() + INTERVAL 3 DAY, NULL, 'Processing', NULL, 119);
 
+-- test6
 -- will not work due to 4AA before insert restriction with the 3 day rule.
-INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
-VALUES (1002, CURDATE(), CURDATE() + INTERVAL 2 DAY, NULL, 'Processing', NULL, 119);
+-- INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
+-- VALUES (1002, CURDATE(), CURDATE() + INTERVAL 2 DAY, NULL, 'Processing', NULL, 119);
 
+-- test7
 -- will not work due to 4AA before insert restriction with the 3 day rule.
-INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
-VALUES (1002, CURDATE(), CURDATE() + INTERVAL 1 DAY, NULL, 'Processing', NULL, 119);
+-- INSERT INTO orders (orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber)
+-- VALUES (1002, CURDATE(), CURDATE() + INTERVAL 1 DAY, NULL, 'Processing', NULL, 119);
 
