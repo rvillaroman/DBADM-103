c484f2ba662fa1857faa1cd1f99a63b6a6a41d2c
ridzrgt <ridz_rigat@dlsu.edu.ph>
2024-11-13 11:47:22 +0800

    Update 5D-Rigat-DBADM-103.sql

--- Reliable DB Development/5D-Rigat-DBADM-103.sql
+++ Reliable DB Development/5D-Rigat-DBADM-103.sql
@@ -4,6 +4,70 @@
 -- Ridz Rigat
 -- ITDBADM_S16
 
+-- new getMSRP
+DROP FUNCTION IF EXISTS getMSRP_2;
+DELIMITER $$
+CREATE FUNCTION getMSRP_2 (param_productCode VARCHAR(15), param_origdate DATE) 
+RETURNS DECIMAL(9,2)
+DETERMINISTIC
+BEGIN
+	DECLARE	var_productcategory	ENUM('C', 'D');
+    DECLARE var_producttype		ENUM('R', 'W');
+    DECLARE var_MSRP			DECIMAL(9,2);
+    DECLARE errormessage		VARCHAR(200);
+    
+	SELECT	product_category
+    INTO	var_productcategory
+    FROM	products
+    WHERE	productCode = param_productCode;
+    
+    -- Check if the product exists
+    IF (var_productcategory IS NULL) THEN
+		SET errormessage := "Product does not exist";
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = errormessage;
+    END IF;
+
+	-- Check if the product is discontinued
+	IF (var_productcategory = 'D') THEN
+		SET errormessage := "Product is discontinued. No MSRP available";
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = errormessage;
+    END IF;
+	
+    -- Check the current products table to determine if wholesale or retail
+    
+    SELECT	product_type
+    INTO	var_producttype
+    FROM	current_products
+    WHERE	productCode = param_productCode;
+    
+    -- Check if the product is retail
+    
+    IF (var_producttype = 'R') THEN
+		-- the product is retail
+        SELECT  MSRP
+        INTO	var_MSRP
+        FROM	product_pricing
+        WHERE	param_origdate BETWEEN startdate AND enddate
+        AND		productCode = param_productCode;
+        
+        -- Check if the price was available
+        IF (var_MSRP IS NULL) THEN
+			SET errormessage := CONCAT("MSRP of the product does not exist yet given the date of ", param_origdate);
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = errormessage;
+        END IF;
+        RETURN var_MSRP;
+    ELSE
+		-- the product is wholesale
+        SELECT 	MSRP
+        INTO	var_MSRP
+        FROM	product_wholesale
+		WHERE	productCode = param_productCode;
+	
+		RETURN var_MSRP;
+    END IF;
+    
+END $$
+DELIMITER ;
 
 DROP TABLE IF EXISTS pricing_variation_report;
 CREATE TABLE pricing_variation_report (
